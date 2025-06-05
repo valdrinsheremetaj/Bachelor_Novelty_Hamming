eed6af4a8d121e22118dc5ac9cef68c3f9aeb618
sheremetajv@gmail.com <sheremetajv@gmail.com>
2025-06-05 11:53:12 +0000

    Implemented Novelty Width Search, added Partial State class, chnaged framework structure

--- src/search/utils/closed.h
+++ src/search/utils/closed.h
@@ -1,31 +1,62 @@
+#ifndef CLOSED_LIST_H
+#define CLOSED_LIST_H
+
 #include <unordered_set>
 #include "../state_registry.h"
+#include "../state_partial.h"
+#include "width_type.h"
 
-// Create partial states for Novelty
-/*Closed list created for Novelty and Hamming Width Search based on Simon Dold's framework*/
-class ClosedList {
-private:
-    std::vector<State> closed; // used for Hamming width search
-    //std::vector<PartialState> partial_closed; // used for Novelty width search later
-public:
-                        // ==== Hamming Width ====
 
-    // Check whether the given state is already in the closed list.
-    bool is_closed(const State& s) const {
-        return std::find(closed.begin(), closed.end(), s) != closed.end();
-    }
-    // Add the given state to the closed list.
-    void mark_closed(const State& s) {
-        if (!is_closed(s))
-            closed.push_back(s);
-
-    }
-    // Clear the closed list.
-    void clear() {
-        closed.clear();
-    }
-    // Get the number of states in the closed list.
-    size_t size() const {
-        return closed.size();
-    }
-};
+class Closed {
+    private:
+        Widthtype width_type;
+        std::vector<State> closed;              // for Hamming width
+        std::vector<PartialState> partial_closed; // for Novelty width
+    
+    public:
+        explicit Closed(Widthtype wt)
+            : width_type(wt) {}
+    
+                            // ==== Common Interface ====
+    
+        void clear() {
+            closed.clear();
+            partial_closed.clear();
+        }
+    
+        size_t size() const {
+            if (width_type == Widthtype::Hamming)
+                return closed.size();
+            else if (width_type == Widthtype::Novelty)
+                return partial_closed.size();
+            return 0;
+        }
+    
+                            // ==== Hamming Width ====
+    
+        bool is_closed(const State &s) const {
+            return width_type == Widthtype::Hamming &&
+                   std::find(closed.begin(), closed.end(), s) != closed.end();
+        }
+    
+        void mark_closed(const State &s) {
+            if (width_type == Widthtype::Hamming && !is_closed(s)) {
+                closed.push_back(s);
+            }
+        }
+    
+                            // ==== Novelty Width ====
+    
+        bool is_closed(const PartialState &ps) const {
+            return width_type == Widthtype::Novelty &&
+                   std::find(partial_closed.begin(), partial_closed.end(), ps) != partial_closed.end();
+        }
+    
+        void mark_closed(const PartialState &ps) {
+            if (width_type == Widthtype::Novelty && !is_closed(ps)) {
+                partial_closed.push_back(ps);
+            }
+        }
+    };
+    
+    #endif
